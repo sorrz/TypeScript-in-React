0b221d62c5f9a1fc5bd9e37393f8da4a5b6e251b
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2022-11-15 14:16:01 +0100

    Final commit for the test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 type TPoint = {
@@ -10,6 +9,7 @@ type TPoint = {
 
 function App() {
   const [points, setPoints] = useState<TPoint[]>([]);
+  const [popped, setPopped] = useState<TPoint[]>([]);
 
   function handlePlaceCircle(e: React.MouseEvent<HTMLDivElement>) {
     const { clientX, clientY } = e;
@@ -19,18 +19,55 @@ function App() {
     } ]);
   }
 
-  
+  function handleUndo() {
+    // Remove last set Point
+    // Check that the array isn't empty to avoid crashes
+    
+      const newPoints = [...points];
+      const poppedPoint = newPoints.pop();
+
+      if (!poppedPoint) return;
+      setPopped([...popped, poppedPoint]);
+      setPoints(newPoints);
+    
+  }
+
+  function handleRedo() {
+    // Copy the Array popped to a newPopped
+    // The Pop the poppedPoint to newPopped.
+      const newPopped = [...popped];
+      const poppedPoint = newPopped.pop();
+    // Make sure the array isn't empty to avoid crash
+      if (!poppedPoint) return;
+      // Set the popped point back to popped array.
+      setPoints([...points, poppedPoint]);
+      setPopped(newPopped);
+
+  }
 
 
   return (
+    <>
+    <div className="top">
+      <button className="buttons" disabled={points.length === 0} onClick={handleUndo}>Undo</button>
+      <button className="buttons" disabled={popped.length === 0} onClick={handleRedo}>Redo</button>
+    </div>
+    
     <div className="App" onClick={handlePlaceCircle}>
-      {points.map(point => ( <div className="point" style= {{
+
+      
+
+      {points.map((point, idx) => ( 
+      <div
+        key={idx}
+        className="point" style= {{
         left: point.x  - 3 + "px",
         top: point.y - 3 + "px", 
       }}>
       
       </div>))}
     </div>
+    </>
   );
 }
 
